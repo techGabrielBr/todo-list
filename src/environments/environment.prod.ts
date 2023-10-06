bb333157c9f3d7e936e39bbe530c0741c1264f3c
Gabriel Oliveira <89689001+techGabrielBr@users.noreply.github.com>
2023-10-06 17:57:47 -0300

    Add static vercel json-server url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_TODO_URL: "https://json-server-todo-p4gqwv2f4-gabriels-projects-943d9c5c.vercel.app/tasks"
+  API_TODO_URL: "https://json-server-todo-rho.vercel.app/tasks"
 };
