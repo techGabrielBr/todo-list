54ce528d7dde09887ea7d3060a1fb672ae2a1333
Gabriel Oliveira <89689001+techGabrielBr@users.noreply.github.com>
2023-10-06 17:19:41 -0300

    Update vercel json-server url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_TODO_URL: "https://json-server-todo-dod8cy6oo-gabriels-projects-943d9c5c.vercel.app/tasks"
+  API_TODO_URL: "https://json-server-todo-p4gqwv2f4-gabriels-projects-943d9c5c.vercel.app/tasks"
 };
